6a945578c27ed0578641b64a26560fac0fcb92f7
rafi075 <hrakibul60@gmail.com>
2024-10-20 13:28:53 -0600

    Added api key

--- contacts.js
+++ contacts.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const sheetUrl = 'https://docs.google.com/spreadsheets/d/16iN-j3xXCV2gyF0XGjaT6jyDOOFwADfqPNTG2YMec4s/edit?usp=sharing';
+
+    
+const apiKey='AIzaSyAkwMkKP5AvvxV0He1AuGEAzEun5-7mb48'// Replace with your API key
+const spreadsheetId = '16iN-j3xXCV2gyF0XGjaT6jyDOOFwADfqPNTG2YMec4s'; // Replace with your spreadsheet ID
+const sheetName = 'Form responses 1'; // Replace with your sheet name
+
+const sheetUrl= `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${apiKey}`
 
     fetch(sheetUrl)
         .then(response => response.text())
